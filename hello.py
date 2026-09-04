361b2550a2ac566849bf0bb239d21d976e08a93d
Fahim <2603052@sit.singaporetech.edu.sg>
2026-09-04 16:44:28 +0800

    First Post pt4. smth fun

--- hello.py
+++ hello.py
@@ -1,21 +1,34 @@
+# print("=============================")
+# print("Welcome here")
+# print("My first post!")
+# print("=============================")
+
+# username = "cool_creator"
+# bio = "Fun Blogger"
+# followers = 100
+
+# print("username: ", username)
+# print("Bio: ", bio)
+# print("Followers: ", followers)
+
+# followers += 50
+# print("Day 1: ", followers)
+
+# followers += 20
+# print("Day 2: ", followers)
+
+# followers -= 10
+# print("Day 3: ", followers)
+
+username = input("Enter Username: ")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
 print("=============================")
-print("Welcome here")
-print("My first post!")
-print("=============================")
+print("Username: ", username)
+print("Age: ", age)
+print("Category: ", category)
 
-username = "cool_creator"
-bio = "Fun Blogger"
-followers = 100
-
-print("username: ", username)
-print("Bio: ", bio)
-print("Followers: ", followers)
-
-followers += 50
-print("Day 1: ", followers)
-
-followers += 20
-print("Day 2: ", followers)
-
-followers -= 10
-print("Day 3: ", followers)
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
