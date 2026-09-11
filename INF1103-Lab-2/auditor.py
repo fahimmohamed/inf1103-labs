3999d9124145ec60453fe01c20524eecfa4d1d32
Fahim <2603052@sit.singaporetech.edu.sg>
2026-09-11 16:38:49 +0800

    3. Accept stock value as integers

--- INF1103-Lab-2/auditor.py
+++ INF1103-Lab-2/auditor.py
@@ -13,3 +13,7 @@ while True:
         print("You have quit!")
         break
 
+    # Accept Stock value as Integers
+    if user_input.isdigit() == False:
+        print("Please enter an integer")
+        failed_entries += 1
