ac06061fa5a692bb77cbe41b4bddc2fc8c2aec8c
Fahim <2603052@sit.singaporetech.edu.sg>
2026-09-11 16:58:56 +0800

    8. Reporting

--- INF1103-Lab-2/auditor.py
+++ INF1103-Lab-2/auditor.py
@@ -1,15 +1,15 @@
 # Initialize Inventory
-invertory = 0
+inventory = 0
 stock_value = 0
 failed_entries = 0
 user_input = ""
 
 while True:
-    user_input = input("Enter the Stock Quantity: ")
+    user_input = input("Enter the Stock Quantity or type 'quit': ")
 
     # Run in a continuous loop asking user to enter a stock quantity
     # until the usertypes quit.
-    if user_input.strip().lower() != "quit":
+    if user_input.strip().lower() == "quit":
         print("You have quit!")
         break
 
@@ -18,14 +18,20 @@ while True:
     if user_input.isdigit() == False:
         print("Please enter a positive integer")
         failed_entries += 1
+        continue
 
     # Accept Stock value as Integers
     stock_value = int(user_input)
 
     # Keep a running total of the inventory
     inventory += stock_value
+    print(f"Successfully processed {stock_value} units. (Current Total: {inventory})")
 
     # Trigger overstock alert
     if inventory > 500:
         print("ALERT: Stock Value exceeded 500 units.")
         break
+
+# Reporting
+print(f"Total Units: {inventory}")
+print(f"Number of Failed Entries: {failed_entries}")
