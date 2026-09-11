cdf1853f501e2776b80cda56678f1604041905a2
Fahim <2603052@sit.singaporetech.edu.sg>
2026-09-11 16:37:12 +0800

    2. Run in continuous Loop

---
+++ INF1103-Lab-2/auditor.py
@@ -0,0 +1,15 @@
+# Initialize Inventory
+invertory = 0
+stock_value = 0
+failed_entries = 0
+user_input = ""
+
+while True:
+    user_input = input("Enter the Stock Quantity: ")
+
+    # Run in a continuous loop asking user to enter a stock quantity
+    # until the usertypes quit.
+    if user_input.strip().lower() != "quit":
+        print("You have quit!")
+        break
+
