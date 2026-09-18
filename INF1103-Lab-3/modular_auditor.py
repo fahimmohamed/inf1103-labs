242810c05eb20f5150e30f1752e374353eb9c63d
Fahim <2603052@sit.singaporetech.edu.sg>
2026-09-18 17:05:37 +0800

    Calculate new total

--- INF1103-Lab-3/modular_auditor.py
+++ INF1103-Lab-3/modular_auditor.py
@@ -3,3 +3,28 @@ inventory = 0
 stock_value = 0
 failed_entries = 0
 user_input = ""
+
+#Handles prompt
+def get_valid_input():
+    #Run in a continuous loop
+    while True:
+        user_input = input("Enter the Stock Quantity or type 'quit': ").strip()
+        
+        if user_input.lower() == "quit":
+            return "quit"
+            
+        if not user_input.isdigit():
+            print("Please enter a positive integer")
+            return "invalid"
+            
+        stock_value = int(user_input)
+        
+        if stock_value < 0:
+            print("Please enter a positive number")
+            return "invalid"
+            
+        return stock_value
+
+#Calculate new total
+def process_delivery(current_total, new_value):
+    return current_total + new_value
