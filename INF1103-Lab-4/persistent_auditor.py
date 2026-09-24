878218d1ce6c7718dce896517338980c0310b00a
Fahim <2603052@sit.singaporetech.edu.sg>
2026-09-24 21:21:51 +0800

    Requirement 1 - Persistence

--- INF1103-Lab-4/persistent_auditor.py
+++ INF1103-Lab-4/persistent_auditor.py
@@ -5,7 +5,37 @@ failed_entries = 0
 deliveries_processed = 0
 total_tax = 0
 user_input = ""
+filename = "inventory.txt"
+transaction_history = []
 
+import os
+
+#Loads inventory upon start up
+def load_inventory():
+    
+    if not os.path.exists(filename):
+        print("Inventory does not exist")
+        return 0, []
+
+    with open(filename, "r") as file:
+        lines = file.read().splitlines()
+
+    if not lines:
+        print("Inventory is empty")
+        return 0, []
+
+    total_inventory = int(lines[0].strip())
+
+    inventory_history = []
+    for line in lines[1:]:
+        if line.strip():
+            inventory_history.append(int(line))
+
+    return total_inventory, inventory_history
+
+
+
+    
 #Handles prompt
 def get_valid_input():
     #Run in a continuous loop
@@ -37,13 +67,22 @@ def calculate_tax(amount):
     return amount * 0.1
 
 #Final summary
-def generate_report(total_units, total_deliveries, failed_attempts, delivery_tax):
+def generate_report(total_units, total_deliveries, failed_attempts, delivery_tax, transaction_history):
     print("\n--- Final Audit Report ---")
     print(f"Total Units in Inventory: {total_units}")
     print(f"Total deliveries processed: {total_deliveries}")
     print(f"Number of Failed Entries: {failed_attempts}")
     print(f"Total tax: {delivery_tax}")
+    print(f"Transaction History: {transaction_history}")
+
     return
+
+#Main loop
+
+inventory, transaction_history = load_inventory()
+
+deliveries_processed = len(transaction_history)
+total_tax = sum(transaction_history) * 0.1
 
 while True:
     result = get_valid_input()
@@ -67,4 +106,6 @@ while True:
     # Update our successful delivery counter
     deliveries_processed += 1
 
-generate_report(inventory, deliveries_processed, failed_entries, total_tax)
+
+
+generate_report(inventory, deliveries_processed, failed_entries, total_tax, transaction_history)
